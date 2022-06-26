b6fb0d5770bfe834c9d5051236a9b4cce43ccf1f
Ben Myers <ben.myers@okstate.edu>
2022-06-26 10:33:09 -0500

    Ben-ify Speedlify

---
+++ _data/sites/ben-myers.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'benmyers.dev', // optional, falls back to object key
+	description: 'Ben’s Personal web site',
+	skip: false,
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use 'run' if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use 'site' if sites are all on the same origin and share assets.
+		freshChrome: 'site'
+	},
+	urls: [
+		'https://benmyers.dev',
+		'https://benmyers.dev/about/',
+		// Popular Posts
+		'https://benmyers.dev/blog/on-the-dl/',
+		'https://benmyers.dev/blog/eleventy-data-cascade/',
+		'https://benmyers.dev/blog/css-can-influence-screenreaders/',
+	]
+};
